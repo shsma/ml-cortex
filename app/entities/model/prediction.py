6f790ee97b82c7e3fcc3c9686bd38becee6156a1
Shady Smaoui <shadysmaoui@hotmail.fr>
2021-02-03 20:47:41 -0500

    update Prediction entities

--- app/entities/model/prediction.py
+++ app/entities/model/prediction.py
@@ -2,8 +2,17 @@ from typing import Dict
 
 
 class Prediction:
-    def __init__(self, brand: Dict[int, int], gender: Dict[int, int], score: Dict[int, float], liked: Dict[int, bool]):
+    def __init__(
+        self, 
+        brand: Dict[int, int]=None,
+        gender: Dict[int, int]=None,
+        score: Dict[int, float]=None,
+        liked: Dict[int, bool]=None,
+        category_ids: Dict[int, str]=None,
+        ):
         self.brand = brand
         self.gender = gender
         self.score = score
         self.liked = liked
+        self.category_ids = category_ids
+
